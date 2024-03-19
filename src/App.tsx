a8140235fd1378253d9c3904d193dd6986be8905
Mayur B Patel <mayur.b.patel@1rivet.com>
2024-03-19 12:56:29 +0530

    Add : login and header page

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import "@mantine/core/styles.css";
-import { Button, MantineProvider } from "@mantine/core";
+import { Flex, MantineProvider } from "@mantine/core";
 
-import { IconArrowLeft } from "@tabler/icons-react";
 import { Route, Routes } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Notifications } from "@mantine/notifications";
+import Login from "./pages/Login";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <MantineProvider>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <IconArrowLeft color="red" size={48} />
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Notifications position="top-center" zIndex={9999} />
 
-      <Routes>
-        <Route path="/" element={<div>Home</div>} />
-        <Route path="/login" element={<div>login</div>} />
-      </Routes>
+      <Flex direction={"column"} h={"100%"}>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Flex>
     </MantineProvider>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { Box, Text } from "@mantine/core";
+
+export function Header() {
+  return (
+    <Box style={{ backgroundColor: "red" }} h={100}>
+      <Text> This is HEader</Text>
+    </Box>
+  );
+}
