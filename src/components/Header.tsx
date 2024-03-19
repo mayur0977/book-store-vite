7dbd0c006ee9807b73388324fca9e61cac845f73
Mayur B Patel <mayur.b.patel@1rivet.com>
2024-03-19 18:42:02 +0530

    Add : mantine with tailwind css and container added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,9 @@ import { Box, Text } from "@mantine/core";
 
 export function Header() {
   return (
-    <Box style={{ backgroundColor: "red" }} h={100}>
+    <Box className="flex justify-between px-4 bg-slate-400" h={100}>
       <Text> This is HEader</Text>
+      <button> This is HEader</button>
     </Box>
   );
 }
