33428fd9b2142ad7899e5853310359698ce98198
Mayur B Patel <mayur.b.patel@1rivet.com>
2024-03-21 15:02:31 +0530

    Add : register page UI

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Divider, Flex, Text, TextInput } from "@mantine/core";
+import { Box, Button, Flex, Text, TextInput } from "@mantine/core";
 import { useNavigate } from "react-router-dom";
 import AuthService from "../core/Auth.service";
 import useNotificationHook from "../shared/useNotificationHook";
@@ -28,70 +28,88 @@ function Register() {
       });
   };
   return (
-    <Flex
-      justify={"center"}
-      align={"center"}
-      direction={"column"}
-      h={"100vh"}
-      gap={48}
-      style={{ borderTop: "8px solid #bd2c1c" }}
-    >
-      <Box
-        p={40}
-        maw={500}
-        mah={400}
-        bg={"#FFFFFF"}
-        style={{
-          border: "1px solid #202010",
-          boxShadow: "-10px 10px #bd2c1c",
-        }}
+    <>
+      <Flex
+        justify={"center"}
+        direction={"column"}
+        align={"center"}
+        h={"100vh"}
+        style={{ borderTop: "8px solid #bd2c1c" }}
       >
-        <Flex direction={"column"} justify={"center"} align={"center"} gap={4}>
-          <Box
-            p={24}
-            style={{ display: "flex", flexDirection: "column", gap: "8px" }}
+        <Box
+          p={{ base: 20, md: 40 }}
+          w={{ base: "100%", md: 500 }}
+          bg={"#FFFFFF"}
+          style={{
+            border: "1px solid #202010",
+            boxShadow: "-10px 10px #bd2c1c",
+          }}
+        >
+          <Flex
+            direction={"column"}
+            justify={"space-between"}
+            align={"center"}
+            h={"100%"}
           >
-            <TextInput w={300} placeholder="Name" />
-            <TextInput w={300} placeholder="Email" />
-            <TextInput placeholder="Password" type={"password"} />
-            <TextInput placeholder="Confirm Password" type={"password"} />
-          </Box>
-          <Button
-            bg={"#020101"}
-            radius={0}
-            color={"#f6f0e6"}
-            tt={"uppercase"}
-            onClick={() => {
-              userRegister();
-            }}
-          >
-            Sign up
-          </Button>
-          <Divider />
-          <Text> or</Text>
-          <Text
-            style={{ color: "#bd2c1c", cursor: "pointer" }}
-            tt={"uppercase"}
-            lts={4}
-            fw={700}
-            onClick={() => {
-              navigate("/login");
-            }}
-          >
-            Log in
-          </Text>
-        </Flex>
-      </Box>
-      <Text
-        style={{ cursor: "pointer" }}
-        td={"underline"}
-        onClick={() => {
-          navigate("/");
-        }}
-      >
-        Go to home page
-      </Text>
-    </Flex>
+            <Box
+              w={"100%"}
+              maw={"300px"}
+              style={{ display: "flex", flexDirection: "column", gap: "12px" }}
+              mb={20}
+            >
+              <TextInput radius={0} w={"100%"} placeholder="Name" />
+              <TextInput radius={0} w={"100%"} placeholder="Email" />
+              <TextInput
+                radius={0}
+                w={"100%"}
+                placeholder="Password"
+                type={"password"}
+              />
+              <TextInput
+                radius={0}
+                w={"100%"}
+                placeholder="Confirm Password"
+                type={"password"}
+              />
+            </Box>
+            <Button
+              bg={"#020101"}
+              radius={0}
+              color={"#f6f0e6"}
+              tt={"uppercase"}
+              onClick={() => {
+                userRegister();
+              }}
+            >
+              Sign up
+            </Button>
+
+            <Text>or</Text>
+            <Text
+              style={{ color: "#bd2c1c", cursor: "pointer" }}
+              tt={"uppercase"}
+              lts={4}
+              fw={700}
+              onClick={() => {
+                navigate("/login");
+              }}
+            >
+              Log in
+            </Text>
+          </Flex>
+        </Box>
+        <Text
+          style={{ cursor: "pointer" }}
+          td={"underline"}
+          onClick={() => {
+            navigate("/");
+          }}
+          mt={48}
+        >
+          Go to home page
+        </Text>
+      </Flex>
+    </>
   );
 }
 
